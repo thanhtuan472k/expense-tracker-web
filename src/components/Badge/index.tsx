ed3f01bea3cf85037a5f095f67c92edc371cd933
tuannt20 <tuango.472000@gmail.com>
2023-08-24 22:36:25 +0700

    :sparkles: feat: Base basic components
    feat: Base basic components

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Badge(props) {
+    return (
+        <span className={`badge badge-${props.type}`}>
+            aaaa
+        </span>
+    )
+}
+
+export default Badge
